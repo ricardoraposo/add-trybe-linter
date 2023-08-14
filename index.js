7729ee14e9d598407341002f918b7188fd519be4
ricardo <ricardorbxx1@gmail.com>
2023-08-14 20:21:36 -0300

    acho que ta funcionando

--- index.js
+++ index.js
@@ -1,34 +1,71 @@
 #!/usr/bin/env node
 
-import fs from "fs";
+const fs = require("fs").promises;
+const { execSync } = require("child_process");
 
-const dependiciesToRemove = [
+const path = "./package.json"
+const eslintConfig = "./.eslintrc.json"
+const dependenciesToRemove = [
   /^.*@typescript-eslint\/eslint-plugin.*$/,
   /^.*@typescript-eslint\/parser.*$/,
   /^.*"eslint".*$/,
   /^.*eslint-plugin-react-hooks.*$/,
   /^.*eslint-plugin-react-refresh.*$/,
-]
+];
 
-const path = "./package.json"
 
-function removeLines(regexList) {
-  fs.readFile(path, 'utf-8', (_, data) => {
-    const lines = data.split('\n');
-    const filteredLines = lines.filter(line => {
-      return !regexList.some(regex => regex.test(line));
-    });
+async function removeLines(regexList) {
+  const data = await fs.readFile(path, 'utf-8')
+  const lines = data.split('\n');
+  const filteredLines = lines.filter(line => {
+    return !regexList.some(regex => regex.test(line));
+  });
 
-    const updatedContent = filteredLines.join('\n');
+  const updatedContent = filteredLines.join('\n');
 
-    fs.writeFile(path, updatedContent, 'utf-8', (err) => {
-      if (err) {
-        console.error('Error writing file:', err);
-      } else {
-        console.log('Lines removed successfully');
-      }
-    });
+  await fs.writeFile(path, updatedContent);
+}
+
+async function createConfigFile() {
+  try {
+    await fs.writeFile(eslintConfig, '{\n"extends": "@trybe/eslint-config-frontend/typescript"\n}');
+    await fs.unlink(".eslintrc.cjs");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function checkViteConfig() {
+  try {
+    await fs.access("vite.config.ts", fs.constants.F_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+function installPackage() {
+  const cmd = "npm install @trybe/eslint-config-frontend -D"
+  execSync(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Erro ao instalar o pacote: ${err.message}`);
+      return;
+    }
+
+    console.log(`Pacote instalado com sucesso!`);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
   })
 }
 
-removeLines(dependiciesToRemove);
+async function main() {
+  if (await checkViteConfig()) {
+    await removeLines(dependenciesToRemove);
+    await createConfigFile();
+    installPackage();
+  } else {
+    console.log("Diretório atual não é um projeto Vite");
+  }
+}
+
+main()
